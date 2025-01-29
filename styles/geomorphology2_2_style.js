2c00110c953f8d8011b0a63070224ae209e8e3a8
chandran457 <archanachandran0707@gmail.com>
2025-01-29 16:18:30 +0530

    Add files via upload

---
+++ styles/geomorphology2_2_style.js
@@ -0,0 +1,65 @@
+var size = 0;
+var placement = 'point';
+function categories_geomorphology2_2(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'Coastal Plain':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(95,155,157,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Lower Plateau (Lateritic) - Dissected':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(175,209,231,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Valley Fill':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(62,142,196,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Water Body':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(105,140,189,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_geomorphology2_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("NAME");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+var style = categories_geomorphology2_2(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement);
+
+    return style;
+};
